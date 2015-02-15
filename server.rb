2bace97bc5baf1674ab13500521ca5d286d25d67
Tyler Nappy <tylernappy@gmail.com>
2015-02-15 15:22:21 -0500

    more if statements and logic with active_record

--- server.rb
+++ server.rb
@@ -7,7 +7,6 @@ require 'twilio-ruby'
 require 'dotenv'
 require "aws/s3"
 require 'active_record'
-
 require 'sinatra/activerecord'
 # require './environments'
 
@@ -47,51 +46,76 @@ post "/" do
    debugger
    counter += 1
    path = "/Users/tylernappy/Documents/photos/imagemagickimage_1_#{counter}.jpg"
-   if !params["MediaUrl0"].nil?
-      image = MiniMagick::Image.open(params["MediaUrl0"])
-      image.combine_options do |c|
-         c.font "Palatino-Roman"
-         # c.font "#{ RAILS_ROOT}/public/images/fonts/georgia.ttf"
-         c.gravity 'South'
-         c.pointsize '100'
-         c.draw "text 0,#{(image.height*c_height).to_i} \"#{params["Body"]}\""
-         c.fill("#FFFFFF")
-         c.draw "text #{shadow_offset},#{(image.height*c_height)+shadow_offset.to_i} \"#{params["Body"]}\""
-         c.fill("blue")
+   member = Member.find_by_phone_number(params["To"])
+   if !member.nil?
+      if params["NumMedia"].to_i == 1
+
+         ##Create image
+         image = MiniMagick::Image.open(params["MediaUrl0"])
+         image.combine_options do |c|
+            c.font "Palatino-Roman"
+            # c.font "#{ RAILS_ROOT}/public/images/fonts/georgia.ttf"
+            c.gravity 'South'
+            c.pointsize '100'
+            c.draw "text 0,#{(image.height*c_height).to_i} \"#{params["Body"]}\""
+            c.fill("#FFFFFF")
+            c.draw "text #{shadow_offset},#{(image.height*c_height)+shadow_offset.to_i} \"#{params["Body"]}\""
+            c.fill("blue")
+         end
+         image.write(path) #saves image with MiniMagick sytnax
+         ##
+
+         ##Save to AWS
+         AWS::S3::S3Object.store(
+            filename,
+            open(path),
+            # open(image), #use image instead so nothing is saved to server end memory
+            ENV['BUCKET'],
+            :access => :public_read
+         )
+         ##
+
+         ##Save to ActiveRecord
+         member.photos.create!({phone_number: params["From"], aws_url: "https://#{ENV['BUCKET']}.s3.amazonaws.com/#{filename}.jpg", confirmed: false})
+         ##
+
+         # response = HTTMultiParty.post("http://192.168.1.135:3000/", :query => {:image => File.binread(path), :counter => counter }) #reads image and POSTs to subsequent server
+         # sends back the new image of what will be displayed on the image
+         client.messages.create(
+            from: params["To"],
+            to: params["From"],
+            body: "Thanks for your image and message!  Here is what it will look like on their ornament.  If you like it, send an *OK* to this number.  If not, resend another image and message!",
+            media_url: "https://#{ENV['BUCKET']}.s3.amazonaws.com/#{filename}.jpg"
+         )
+      elsif  params["NumMedia"].to_i == 0 && params["Body"].downcase == "redo"
+
+      elsif params["NumMedia"].to_i == 0 && params["Body"].downcase == "ok"
+         #Do stuff if it sender wants it to be posted
+         #Look up URL and ip address of pi
+         photo = member.photos.find_by_phone_number(params["From"]).last
+         res = HTTMultiParty.get(photo.aws_url) #get binary from image
+         response = HTTMultiParty.post(member.ip, :query => {:image => res.parsed_response, :counter => counter })#post binary to raspberry pi
+      else
+         puts "No image sent try again"
+         # sends error message to original sender
+         client.messages.create(
+            from: params["To"],
+            to: params["From"],
+            body: "Oops!  Looked like you didn't include an image.  In the same message, send both an image and a message you would like displayed on the image."
+         )
       end
-      image.write(path) #saves image with MiniMagick sytnax
-      ## Jason's part
-      AWS::S3::S3Object.store(
-         filename,
-         open(path),
-         # open(image), #use image instead so nothing is saved to server end memory
-         ENV['BUCKET'],
-         :access => :public_read
-      )
-      ##
-      # response = HTTMultiParty.post("http://192.168.1.135:3000/", :query => {:image => File.binread(path), :counter => counter }) #reads image and POSTs to subsequent server
-      # sends back the new image of what will be displayed on the image
-      client.messages.create(
-         from: params["To"],
-         to: params["From"],
-         body: "Thanks for your image and message!  Here is what it will look like on their ornament.  If you like it, send an *OK* to this number.  If not, resend another image and message!",
-         media_url: "https://#{ENV['BUCKET']}.s3.amazonaws.com/#{filename}"
-      )
-   elsif params["MediaUrl0"].nil? && params["Body"].downcase == "ok"
-      #Do stuff if it sender wants it to be posted
-      #Look up URL and ip address of pi
-      res = HTTMultiParty.get(url) #get binary from image
-      response = HTTMultiParty.post(ip, :query => {:image => res.parsed_response, :counter => counter })#post binary to raspberry pi
+      # counter += 1
    else
-      puts "No image sent try again"
-      # sends error message to original sender
       client.messages.create(
-         from: params["To"],
-         to: params["From"],
-         body: "Oops!  Looked like you didn't include an image.  In the same message, send both an image and a message you would like displayed on the image."
+      from: params["To"],
+      to: params["From"],
+      body: "Oops! Looks like your friends eFrame is experiencing some technical difficulties. We will inform them of this."
       )
    end
-   # counter += 1
+end
+
+def create_image url
+
 end
 
 # image = HTTMultiParty.get(params["MediaUrl0"])
